d0cb1a9690c7b3a1876f4d4f068a590e2a7fd707
Carlos Gonzalez <carlosgj94@gmail.com>
2017-02-20 16:45:15 +0200

    A bit of refactoring

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,8 @@ fn main() {
     window.set_title("First GTK+ Program");
     window.set_default_size(950, 700);
 
-    //Stack and StackSwitcher
-    let stack = Stack::new();
-    stack.add_titled(&Label::new("Programs"), "Programs", "Programs");
-    stack.add_titled(&Label::new("Firewall"), "Firewall", "Firewall");
-    stack.add_titled(&Label::new("Forbiden webs"), "Forbiden webs", "Forbiden webs");
-
-    let stack_switcher = StackSwitcher::new();
-    stack_switcher.set_stack(Some(&stack));
-        
-    //Main box
-    let v_box = Box::new(Orientation::Vertical, 10);
-    let h_box = Box::new(Orientation::Horizontal, 50);
-
-
-    h_box.pack_start(&stack_switcher, true, false, 50);
-    v_box.pack_start(&h_box, false, false, 20);
-    v_box.pack_start(&stack, true, true, 20);
-
     //Add the box to the window
-    window.add(&v_box);
+    window.add(&box_structure());
     window.show_all();
 
     window.connect_delete_event(|_, _| {
@@ -41,9 +23,32 @@ fn main() {
         Inhibit(false)
     });
 
-    /*button.connect_clicked(|_| {
-        println!("Clicked!");
-    });*/
-
     gtk::main();
+}
+
+fn box_structure() -> Box {
+    //Stack and StackSwitcher
+    
+    let stack_switcher = StackSwitcher::new();
+    let stack = stack_manager();
+    stack_switcher.set_stack(Some(&stack));
+        
+    //Main box
+    let v_box = Box::new(Orientation::Vertical, 10);
+    let h_box = Box::new(Orientation::Horizontal, 50);
+
+    h_box.pack_start(&stack_switcher, true, false, 50);
+    v_box.pack_start(&h_box, false, false, 15);
+    v_box.pack_start(&stack, true, true, 20);
+   
+    return v_box;
+}
+
+fn stack_manager() -> Stack {
+    let stack = Stack::new();
+    stack.add_titled(&Label::new("Programs"), "Programs", "Programs");
+    stack.add_titled(&Label::new("Firewall"), "Firewall", "Firewall");
+    stack.add_titled(&Label::new("Forbiden webs"), "Forbiden webs", "Forbiden webs");
+
+    return stack;
 }
