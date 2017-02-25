32bf694015771010feb9a7f8039b4abde7142e25
Carlos Gonzalez <carlosgj94@gmail.com>
2017-02-25 20:23:50 +0200

    Added a headerbar and added the stackswitcher inside of it

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate gtk;
 extern crate gdk;
 
 use gtk::prelude::*;
-use gtk::{Window, WindowType, Stack, StackSwitcher, Box, Orientation};
+use gtk::{Window, WindowType, Stack, StackSwitcher, Box, Orientation, HeaderBar};
 
 mod programs;
 mod firewall;
@@ -17,9 +17,32 @@ fn main() {
     let window = Window::new(WindowType::Toplevel);
     window.set_title("First GTK+ Program");
     window.set_default_size(950, 700);
+    
+    //Header
+    let header = HeaderBar::new();
+    header.set_show_close_button(true);
+    window.set_titlebar(Some(&header));
 
+    //Basic structure
+    let stack_switcher = StackSwitcher::new();
+    let stack = stack_manager();
+    stack_switcher.set_stack(Some(&stack));
+    
+    //Box to center the header
+    let header_box = Box::new(Orientation::Horizontal, 0);
+    header_box.pack_start(&stack_switcher, true, false, 250);
+
+    //Main box
+    let v_box = Box::new(Orientation::Vertical, 10);
+    let h_box = Box::new(Orientation::Horizontal, 50);
+
+    //h_box.pack_start(&stack_switcher, true, false, 50);
+    header.pack_start(&header_box);
+    v_box.pack_start(&h_box, false, false, 15);
+    v_box.pack_start(&stack, true, true, 20);
+   
     //Add the box to the window
-    window.add(&box_structure());
+    window.add(&v_box);
     window.show_all();
 
     window.connect_delete_event(|_, _| {
@@ -28,24 +51,6 @@ fn main() {
     });
 
     gtk::main();
-}
-
-fn box_structure() -> Box {
-    //Stack and StackSwitcher
-    
-    let stack_switcher = StackSwitcher::new();
-    let stack = stack_manager();
-    stack_switcher.set_stack(Some(&stack));
-        
-    //Main box
-    let v_box = Box::new(Orientation::Vertical, 10);
-    let h_box = Box::new(Orientation::Horizontal, 50);
-
-    h_box.pack_start(&stack_switcher, true, false, 50);
-    v_box.pack_start(&h_box, false, false, 15);
-    v_box.pack_start(&stack, true, true, 20);
-   
-    return v_box;
 }
 
  fn stack_manager() -> Stack {
