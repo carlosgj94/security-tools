2a6256a32c13c8a27624b46626028ab090989570
Carlos Gonzalez <carlosgj94@gmail.com>
2017-02-25 19:29:11 +0200

    Hosts stack now shows the entries on the file as label in the program

--- src/hosts/mod.rs
+++ src/hosts/mod.rs
@@ -7,32 +7,46 @@ use gtk::{Label, Box, Orientation};
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufReader;
 
 pub fn get_box() -> Box {
     let v_box = Box::new(Orientation::Vertical, 10);
     let h_box = Box::new(Orientation::Horizontal, 10);
 
-    h_box.pack_start(&Label::new("Forbiden Webs"), true, false, 0);
+    let file_strings = get_hosts_file();
+    let label_box = get_items_box(file_strings);
+    
+    h_box.pack_start(&label_box, true, false, 0);
     v_box.pack_start(&h_box, false, false, 0);
-    let file_string = get_hosts_file();
-    println!("{}", &file_string);
 
     return v_box;
 }
 
-fn get_hosts_file() -> String {
-    // Open the path in read-only mode, returns `io::Result<File>`
-    let mut file = match File::open("/etc/hosts") {
+fn get_hosts_file() -> Vec<String> {
+    let file = match File::open("/etc/hosts"){
         // The `description` method of `io::Error` returns a string that
         // describes the error
-        Err(why) => panic!("couldn't open '/etc/hosts': {}",why.description()),
+        Err(why) => panic!("couldn't open {}: {}", "/etc/hosts",
+                                                   why.description()),
         Ok(file) => file,
     };
-
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read '/etc/hosts': {}", why.description()),
-        Ok(_) => print!("'/etc/hosts' contains:\n{}", s),
+    let f = BufReader::new(file);
+    let mut v = Vec::new();
+    for line in f.lines() {
+            let l = line.unwrap();
+            v.push(l);
     }
-    return s;
+
+    return v;
+}
+
+fn get_items_box(lines: Vec<String>) -> Box {
+    
+    let v_box = Box::new(Orientation::Vertical, 10);
+    for item in lines {
+        let i: &str = &item;
+        let label = Label::new(Some(i));
+        v_box.pack_start(&label, true, false, 0);
+    }
+    return v_box;
 }
